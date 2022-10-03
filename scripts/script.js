49413cb82bf4ab456f8bc1e95f1eec14cc4b9aef
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-03 11:41:02 +0600

    feat:  calculating sum and avg
    and  displaying  them on HTML page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+var sum = 0;
+var avg = 0;
+var count = 0;
+
 var inputElement = document.getElementById("number");
 inputElement.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
@@ -5,16 +9,47 @@ inputElement.addEventListener("keydown", function (e) {
     }
 });
 function process() {
-    inputNumber = document.getElementById("number");
-    addNumberToDisplay(inputNumber.value);
-    console.log(inputNumber.value);
-    inputNumber.value = "";
+    inputNumberElement = document.getElementById("number");
+    addNumberToDisplay(inputNumberElement.value);
+    sum  = calculateSum(inputNumberElement.value, sum);
+    updateSumResult();
+    increaseCount();
+    updateAvg();
+    inputNumberElement.value = "";
 }
 
 function addNumberToDisplay(number) {
-    ul_element = document.getElementById("added_numbers");
-    var li_number = document.createElement('li');
-    li_number.appendChild(document.createTextNode(number));
+    ulElement = document.getElementById("added_numbers");
+    liNumber = document.createElement('li');
+    liNumber.appendChild(document.createTextNode(number));
 
-    ul_element.appendChild(li_number);
+    ulElement.appendChild(liNumber);
+}
+
+function calculateSum(number, sum) {
+    newSum = Number(number) + sum;
+    return newSum
+}
+
+function updateSumResult() {
+    sumResultElement = document.getElementById("sum_result");
+    sumResultElement.innerText = sum;
+}
+
+function increaseCount() {
+    count += 1;
+
+    console.log(count);
+}
+
+function calculateAvg() {
+    avg = sum / count;
+
+    return avg;
+}
+
+function updateAvg() {
+    calculateAvg();
+    avgResultElement = document.getElementById("avg_result");
+    avgResultElement.innerText =  avg;
 }
