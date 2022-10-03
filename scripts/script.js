7a21c8cf0049935538b1a8396e1514b200681269
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-03 10:52:45 +0600

    rename: processNumber  -> process

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 var inputElement = document.getElementById("number");
 inputElement.addEventListener("keydown", function(e) {
     if (e.key == "Enter") {
-        processNumber();
+        process();
     }
 });
-function processNumber() {
+function process() {
     inputNumber = document.getElementById("number");
     console.log(inputNumber.value);
     inputNumber.value = "";
