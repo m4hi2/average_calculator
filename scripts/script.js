1a964cc2eb0d772a84f52f4337bd796749cf5f6b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-03 10:59:40 +0600

    feat: displaying added numbers to the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
 var inputElement = document.getElementById("number");
-inputElement.addEventListener("keydown", function(e) {
+inputElement.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
         process();
     }
 });
 function process() {
     inputNumber = document.getElementById("number");
+    addNumberToDisplay(inputNumber.value);
     console.log(inputNumber.value);
     inputNumber.value = "";
+}
+
+function addNumberToDisplay(number) {
+    ul_element = document.getElementById("added_numbers");
+    var li_number = document.createElement('li');
+    li_number.appendChild(document.createTextNode(number));
+
+    ul_element.appendChild(li_number);
 }
